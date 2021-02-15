b7bec983c2beb0d6c5975e2829272a7e5a01a0f6
Matthes <matthes@leuffen.de>
2021-02-15 14:30:21 +0100

    update

--- src/Type/RouteParams.php
+++ src/Type/RouteParams.php
@@ -7,25 +7,20 @@ namespace Brace\Router\Type;
 class RouteParams
 {
 
-    private $params = [];
 
-    public function __construct (array $params)
-    {
-
-    }
+    public function __construct (
+        private array $params = []
+    ){}
 
     public function has(string $key) : bool
     {
         return isset ($this->params[$key]);
     }
 
-    public function get(string $key, $default=null) : string
+    public function get(string $key, $default=null) : ?string
     {
         if ( ! isset ($this->params[$key])) {
-            if ($default instanceof \Exception)
-                throw $default;
-            if ($default === null)
-                throw new \InvalidArgumentException("RouteParam '$key' not existing.");
+            return null;
         }
         return $this->params[$key] ?? $default;
     }
